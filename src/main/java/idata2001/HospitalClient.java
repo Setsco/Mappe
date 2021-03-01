511315d8b7ba3a4f16a1712b3f0160bbaaae7eb4
Frank <francig@stud.ntnu.no>
2021-03-01 14:05:12 +0100

    seven

--- src/main/java/idata2001/HospitalClient.java
+++ src/main/java/idata2001/HospitalClient.java
@@ -1,5 +1,32 @@
 package idata2001;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class HospitalClient {
-    
+
+    public HospitalClient(){
+        Hospital KillThemAll = new Hospital("KillThemAll");
+        HospitalTestData.fillRegisterWithTestData(KillThemAll);
+        ArrayList<Patient> patients = new ArrayList<>();
+        ArrayList<Employee> employees = new ArrayList<>();
+    }
+
+    public void removePatient(){
+        String socialSecurityNumber;
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Delete patient by security number");
+        System.out.println("Enter security number");
+        socialSecurityNumber = sc.nextLine();
+        
+    }
+
+
+
+    public static void main(String[] args) {
+        removePatient();
+        removeEmployee();
+
+    }
 }
--- src/main/java/idata2001/RemoveException.java
+++ src/main/java/idata2001/RemoveException.java
@@ -1,7 +1,7 @@
 package idata2001;
 
 public class RemoveException extends Exception {
-    private long serialVersionUID = 1L;
+    private static long serialVersionUID = 1L;
 
     public RemoveException(String exception){
         super(exception);
